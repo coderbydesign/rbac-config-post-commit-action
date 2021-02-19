5d862d0619630046b5278aee6f02913135eb508f
Keith Walsh <kwalsh@redhat.com>
2021-02-19 12:09:43 -0500

    Specify workspace

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 GITHUB_TOKEN=$1
 BRANCH_NAME=$2
+WORKSPACE=/github/workspace
 
 if [ -z "${GITHUB_TOKEN}" ]; then
     echo "error: no GITHUB_TOKEN supplied"
@@ -12,10 +13,10 @@ if [ -z "${BRANCH_NAME}" ]; then
    export BRANCH_NAME=master
 fi
 
-touch model-access-permissions.configmap.yml
-kubectl create configmap model-access-permissions --from-file ./configs/permissions --dry-run=client --validate=false -o yaml > model-access-permissions.configmap.yml
+touch "${WORKSPACE}/model-access-permissions.configmap.yml"
+kubectl create configmap model-access-permissions --from-file "${WORKSPACE}/configs/permissions" --dry-run=client --validate=false -o yaml > "${WORKSPACE}/model-access-permissions.configmap.yml"
 echo '  annotations:
-    qontract.recycle: "true"' >> model-access-permissions.configmap.yml
+    qontract.recycle: "true"' >> "${WORKSPACE}/model-access-permissions.configmap.yml"
 
 
 remote_repo="https://${GITHUB_ACTOR}:${GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
